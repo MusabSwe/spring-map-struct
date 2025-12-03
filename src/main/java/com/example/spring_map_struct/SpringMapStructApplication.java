c3fb9ad20aff4ce1108a7c8d967ff057733e3016
musab <malqarninet@outlook.com>
2025-12-03 14:09:49 +0300

    project starter

---
+++ src/main/java/com/example/spring_map_struct/SpringMapStructApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_map_struct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMapStructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMapStructApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_map_struct/SpringMapStructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_map_struct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMapStructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
